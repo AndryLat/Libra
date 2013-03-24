684366e0537073b6f895a6ad1251d74d7c816450
Konstantin Kuyun <disapp0int@gmail.com>
2013-03-24 18:16:22 +0200

    Аякс в отдельном классе, изменена структура анкеты, добавлена поддержка транзакций

--- src/com/netcracker/libra/controller/RegController.java
+++ src/com/netcracker/libra/controller/RegController.java
@@ -12,6 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,32 +31,61 @@ public class RegController {
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String showForm(@ModelAttribute("regForm") RegisterForm form,
-			ModelMap model) {
-		model.addAttribute("regForm", form);
-		model.put("uniList", RegformService.getUniversityList());
-		model.put("facList", RegformService.getFacultyList());
-		model.put("deptList", RegformService.getDepartmentList());
-		model.put("languages", RegformService.getProgrammingLanguagesList());
+	public String showForm(@ModelAttribute("regForm") RegisterForm form, ModelMap model) {
+		model.put("regForm", form);
+		//model.put("univers", RegformService.getUniversityList());
 		return "/signup/appform";
 	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String renderForm(@ModelAttribute("regForm") RegisterForm form, @RequestParam("fact") int facultyId, @RequestParam("dept") int departmentId, ModelMap model) {
+		return "/signup/appform";
+	}
+	
+	@RequestMapping(value = "/welcome", method=RequestMethod.GET)
+	public String showWelcomePage(){
+			return "redirect:/register.html";
+	}
+	
+	@RequestMapping(value = "/welcome", method=RequestMethod.POST)
+	public String showWelcomePageOnSubmit(@ModelAttribute("regForm") @Valid RegisterForm form, BindingResult result, ModelMap model) {
+		
+		if (result.hasErrors()) {
+			model.put("regForm", form);
+			return "redirect:/register.html";
+		}
+		else
+			form.setUserId(RegformService.getUserId());
+			form.setAppId(RegformService.getAppformId());
+			form.setTemplateId(RegformService.getActiveTemplateId());
+			RegformService.registerUser(form);
+			return "signup/welcome";
+	}
 
-	@RequestMapping("/next")
-	public String validateForm(@Valid RegisterForm form, BindingResult result,
-			MultipartFile photo, ModelMap model) {
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public String showAppForm(@ModelAttribute("regForm") RegisterForm form, ModelMap model) {
+		model.addAttribute("columns", RegformService.getActiveTemplate());
+		form.setTemplateId(RegformService.getActiveTemplateId());
+		model.put("regForm", form);
+		return "signup/showAppForm";
+	}
+	
+	@RequestMapping(value = "/signup", method=RequestMethod.POST)
+	public String validateForm(RegisterForm form, BindingResult result, MultipartFile photo, ModelMap model) {
 
 		if (result.hasErrors()) {
 			result.getFieldError().toString();
 			return "redirect:/register.html";
+			
 		} else {
-
 			form.setAppId(RegformService.getAppformId());
+			
 			if (!photo.isEmpty()) {
 				String orgName = photo.getOriginalFilename();
 				String filePath = form.getAppId() + ".png";
 				File dest = new File(filePath);
 
-				try {
+				try { 
 					photo.transferTo(dest);
 				} catch (IllegalStateException e) {
 					e.printStackTrace();
@@ -65,52 +95,29 @@ public class RegController {
 					return "File uploaded failed:" + orgName;
 				}
 			}
-			model.addAttribute("regForm", form);
-			return "redirect:/register/showAppForm.html";
+			form.setGeneratedCode(ConfirmationCodeGenerator.generateCode());
+			form.setUserId(RegformService.getUserId());
+			return "signup/review";
 		}
 	}
 
-	@RequestMapping(value = "/showAppForm", method = RequestMethod.GET)
-	public String showAppForm(ModelMap model,
-			@ModelAttribute("regForm") RegisterForm form) {
-		model.addAttribute("columns", RegformService.getActiveTemplate());
-		form.setTemplateId(RegformService.getActiveTemplateId());
-		model.addAttribute("regForm", form);
-		return "signup/showAppForm";
-	}
-
-	@RequestMapping(value = "/submit", method = RequestMethod.POST)
-	public String submit(ModelMap model,
-			@ModelAttribute("regForm") RegisterForm form) {
-		form.setGeneratedCode(ConfirmationCodeGenerator.generateCode());
-		form.setUserId(RegformService.getUserId());
-		model.addAttribute("regForm", form);
-		return "signup/review";
-	}
-
-	@RequestMapping(value = "/submit", method = RequestMethod.GET)
-	public String reviewForm(ModelMap model,
-			@ModelAttribute("regForm") RegisterForm form) {
-		return "signup/review";
-	}
-
 	@RequestMapping(value = "/verifyCode", method = RequestMethod.POST)
-	public String verifyCode(@ModelAttribute("regForm") RegisterForm form,
-			ModelMap model) throws SQLException {
+	public String verifyCode(@ModelAttribute("regForm") RegisterForm form, ModelMap model) throws SQLException {
 
 		if (form.getEnteredCode().equals(form.getGeneratedCode())) {
 			System.out.println("Comparing " + form.getEnteredCode() + " and "
 					+ form.getGeneratedCode());
 			form.setAppId(RegformService.getAppformId());
-			RegformService.registerUser(form);
+			model.put("regForm", form);
 			return "signup/success";
 		}
 
-		else
+		else {
 			System.out.println("Comparing " + form.getEnteredCode() + " and "
 					+ form.getGeneratedCode());
 		model.addAttribute("regForm", form);
 		return "redirect:/register/submit.html";
+		}
 	}
 
 }
--- src/com/netcracker/libra/service/RegformService.java
+++ src/com/netcracker/libra/service/RegformService.java
@@ -1,34 +1,36 @@
 package com.netcracker.libra.service;
 
-import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
+
+import org.springframework.transaction.annotation.Transactional;
 
 import com.netcracker.libra.dao.AppFormJDBC;
 import com.netcracker.libra.dao.ColumnJDBC;
+import com.netcracker.libra.dao.HrJDBC;
 import com.netcracker.libra.model.AppFormColumns;
+import com.netcracker.libra.model.Department;
+import com.netcracker.libra.model.Faculty;
 import com.netcracker.libra.model.RegisterForm;
+import com.netcracker.libra.model.University;
+import com.netcracker.libra.util.security.Security;
 
 public class RegformService {
 	
 	private static AppFormJDBC jdbc = new AppFormJDBC();
 	private static ColumnJDBC cjdbc = new ColumnJDBC();
+	private static HrJDBC hjdbc = new HrJDBC();
 	
-	private static Map<Integer, String> universities = jdbc.getAllUniversities();
-	private static Map<Integer, String> faculties = jdbc.getAllFaculties(); 
-	private static Map<Integer, String> departments = jdbc.getAllDepartments();
-	
-	public static Map<Integer, String> getUniversityList() {
-		return universities;
+	public static List<University> getUniversityList() {
+		return hjdbc.getAllUniversity();
 	}
 	
-	public static Map<Integer, String> getFacultyList() {
-		return faculties;
+	public static List<Faculty> getFacultyListByUniversityID(int univerId) {
+		return hjdbc.getAllFaculties(univerId);
 	}
 	
-	public static Map<Integer, String> getDepartmentList() {
-		return departments;
-	}
+	public static List<Department> getDepartmentListByFacultyID(int facultyId) {
+		return hjdbc.getAllDepartments("f.facultyid", facultyId);
+	}	
 
 	public static Long getAppformId() {
 		return jdbc.getAppformNextVal();
@@ -46,12 +48,11 @@ public class RegformService {
 		return jdbc.queryForActiveTemplateId();
 	}
 	
-	public static Map getProgrammingLanguagesList() {
-		return jdbc.queryForProgrammingLanguages();
+	@Transactional
+	public static void registerUser(RegisterForm form) {
+		jdbc.createNewUserAsStudent(form.getUserId(), form.getName(), form.getLastName(), Security.getMD5hash(form.getPassword()), form.getEmail());
+		jdbc.insertAppFormDetails(form.getAppId(), form.getUserId(), form.getPatronymic(), form.getPhoneNumber(), form.getDepartment(), 1, form.getCourse(), form.getGraduated(), form.getTemplateId());
 	}
 	
-	public static void registerUser(RegisterForm form) throws SQLException {
-		jdbc.fillAppForm(form);
-	}
 
 }
